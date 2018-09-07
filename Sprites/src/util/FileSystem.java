949766889285d9dbca7cb8813183b00ebe4dfec2
mnlbk <manal.bekao@gmail.com>
2018-09-07 01:55:34 +0100

    Update FileSystem.java

--- Sprites/src/util/FileSystem.java
+++ Sprites/src/util/FileSystem.java
@@ -1,5 +1,10 @@
-package util;
+package fr.ensibs.fs;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 public interface FileSystem {
-
+  
+	public InputStream inputStreamProvider(String path);
+		
 }
